a32de55575974fbbcf2123260a2794ae7f797fc2
Ryan Jones <ryanjones33@gmail.com>
2019-07-24 10:50:20 -0700

    Tax Data

---
+++ src/data/taxInfo.js
@@ -0,0 +1,308 @@
+export const taxInfo = {
+  "Alabama": {
+     "effective_real_estate_tax_rate": 0.0042,
+     "annual_taxes_on_194k_home": 817,
+     "median_home_value": 132100,
+     "annual_taxes_median_home_value": 558
+  },
+  "Alaska": {
+     "effective_real_estate_tax_rate": 0.0119,
+     "annual_taxes_on_194k_home": 2303,
+     "median_home_value": 261900,
+     "annual_taxes_median_home_value": 3117
+  },
+  "Arizona": {
+     "effective_real_estate_tax_rate": 0.0072,
+     "annual_taxes_on_194k_home": 1400,
+     "median_home_value": 193200,
+     "annual_taxes_median_home_value": 1398
+  },
+  "Arkansas": {
+     "effective_real_estate_tax_rate": 0.0063,
+     "annual_taxes_on_194k_home": 1213,
+     "median_home_value": 118500,
+     "annual_taxes_median_home_value": 743
+  },
+  "California": {
+     "effective_real_estate_tax_rate": 0.0077,
+     "annual_taxes_on_194k_home": 1490,
+     "median_home_value": 443400,
+     "annual_taxes_median_home_value": 3414
+  },
+  "Colorado": {
+     "effective_real_estate_tax_rate": 0.0055,
+     "annual_taxes_on_194k_home": 1065,
+     "median_home_value": 286100,
+     "annual_taxes_median_home_value": 1575
+  },
+  "Connecticut": {
+     "effective_real_estate_tax_rate": 0.0207,
+     "annual_taxes_on_194k_home": 3999,
+     "median_home_value": 270100,
+     "annual_taxes_median_home_value": 5582
+  },
+  "Delaware": {
+     "effective_real_estate_tax_rate": 0.0056,
+     "annual_taxes_on_194k_home": 1078,
+     "median_home_value": 238600,
+     "annual_taxes_median_home_value": 1329
+  },
+  "District of Columbia": {
+     "effective_real_estate_tax_rate": 0.0055,
+     "annual_taxes_on_194k_home": 1055,
+     "median_home_value": 537400,
+     "annual_taxes_median_home_value": 2930
+  },
+  "Florida": {
+     "effective_real_estate_tax_rate": 0.0098,
+     "annual_taxes_on_194k_home": 1897,
+     "median_home_value": 178700,
+     "annual_taxes_median_home_value": 1752
+  },
+  "Georgia": {
+     "effective_real_estate_tax_rate": 0.0091,
+     "annual_taxes_on_194k_home": 1769,
+     "median_home_value": 158400,
+     "annual_taxes_median_home_value": 1448
+  },
+  "Hawaii": {
+     "effective_real_estate_tax_rate": 0.0027,
+     "annual_taxes_on_194k_home": 525,
+     "median_home_value": 563900,
+     "annual_taxes_median_home_value": 1529
+  },
+  "Idaho": {
+     "effective_real_estate_tax_rate": 0.0075,
+     "annual_taxes_on_194k_home": 1447,
+     "median_home_value": 176800,
+     "annual_taxes_median_home_value": 1322
+  },
+  "Illinois": {
+     "effective_real_estate_tax_rate": 0.0231,
+     "annual_taxes_on_194k_home": 4476,
+     "median_home_value": 179700,
+     "annual_taxes_median_home_value": 4157
+  },
+  "Indiana": {
+     "effective_real_estate_tax_rate": 0.0087,
+     "annual_taxes_on_194k_home": 1679,
+     "median_home_value": 130200,
+     "annual_taxes_median_home_value": 1130
+  },
+  "Iowa": {
+     "effective_real_estate_tax_rate": 0.0153,
+     "annual_taxes_on_194k_home": 2960,
+     "median_home_value": 137200,
+     "annual_taxes_median_home_value": 2099
+  },
+  "Kansas": {
+     "effective_real_estate_tax_rate": 0.014,
+     "annual_taxes_on_194k_home": 2713,
+     "median_home_value": 139200,
+     "annual_taxes_median_home_value": 1952
+  },
+  "Kentucky": {
+     "effective_real_estate_tax_rate": 0.0086,
+     "annual_taxes_on_194k_home": 1667,
+     "median_home_value": 130000,
+     "annual_taxes_median_home_value": 1120
+  },
+  "Louisiana": {
+     "effective_real_estate_tax_rate": 0.0052,
+     "annual_taxes_on_194k_home": 1006,
+     "median_home_value": 152900,
+     "annual_taxes_median_home_value": 795
+  },
+  "Maine": {
+     "effective_real_estate_tax_rate": 0.0135,
+     "annual_taxes_on_194k_home": 2619,
+     "median_home_value": 179900,
+     "annual_taxes_median_home_value": 2435
+  },
+  "Maryland": {
+     "effective_real_estate_tax_rate": 0.011,
+     "annual_taxes_on_194k_home": 2126,
+     "median_home_value": 296500,
+     "annual_taxes_median_home_value": 3257
+  },
+  "Massachusetts": {
+     "effective_real_estate_tax_rate": 0.0122,
+     "annual_taxes_on_194k_home": 2365,
+     "median_home_value": 352600,
+     "annual_taxes_median_home_value": 4309
+  },
+  "Michigan": {
+     "effective_real_estate_tax_rate": 0.0164,
+     "annual_taxes_on_194k_home": 3179,
+     "median_home_value": 136400,
+     "annual_taxes_median_home_value": 2241
+  },
+  "Minnesota": {
+     "effective_real_estate_tax_rate": 0.0115,
+     "annual_taxes_on_194k_home": 2225,
+     "median_home_value": 199700,
+     "annual_taxes_median_home_value": 2296
+  },
+  "Mississippi": {
+     "effective_real_estate_tax_rate": 0.008,
+     "annual_taxes_on_194k_home": 1556,
+     "median_home_value": 109300,
+     "annual_taxes_median_home_value": 879
+  },
+  "Missouri": {
+     "effective_real_estate_tax_rate": 0.0099,
+     "annual_taxes_on_194k_home": 1910,
+     "median_home_value": 145400,
+     "annual_taxes_median_home_value": 1435
+  },
+  "Montana": {
+     "effective_real_estate_tax_rate": 0.0084,
+     "annual_taxes_on_194k_home": 1625,
+     "median_home_value": 209100,
+     "annual_taxes_median_home_value": 1756
+  },
+  "Nebraska": {
+     "effective_real_estate_tax_rate": 0.018,
+     "annual_taxes_on_194k_home": 3485,
+     "median_home_value": 142400,
+     "annual_taxes_median_home_value": 2565
+  },
+  "Nevada": {
+     "effective_real_estate_tax_rate": 0.0069,
+     "annual_taxes_on_194k_home": 1331,
+     "median_home_value": 216400,
+     "annual_taxes_median_home_value": 1489
+  },
+  "New Hampshire": {
+     "effective_real_estate_tax_rate": 0.022,
+     "annual_taxes_on_194k_home": 4257,
+     "median_home_value": 244900,
+     "annual_taxes_median_home_value": 5388
+  },
+  "New Jersey": {
+     "effective_real_estate_tax_rate": 0.0244,
+     "annual_taxes_on_194k_home": 4725,
+     "median_home_value": 321100,
+     "annual_taxes_median_home_value": 7840
+  },
+  "New Mexico": {
+     "effective_real_estate_tax_rate": 0.0078,
+     "annual_taxes_on_194k_home": 1502,
+     "median_home_value": 163900,
+     "annual_taxes_median_home_value": 1272
+  },
+  "New York": {
+     "effective_real_estate_tax_rate": 0.0168,
+     "annual_taxes_on_194k_home": 3246,
+     "median_home_value": 293000,
+     "annual_taxes_median_home_value": 4915
+  },
+  "North Carolina": {
+     "effective_real_estate_tax_rate": 0.0086,
+     "annual_taxes_on_194k_home": 1656,
+     "median_home_value": 161000,
+     "annual_taxes_median_home_value": 1378
+  },
+  "North Dakota": {
+     "effective_real_estate_tax_rate": 0.0101,
+     "annual_taxes_on_194k_home": 1962,
+     "median_home_value": 174100,
+     "annual_taxes_median_home_value": 1765
+  },
+  "Ohio": {
+     "effective_real_estate_tax_rate": 0.0157,
+     "annual_taxes_on_194k_home": 3042,
+     "median_home_value": 135100,
+     "annual_taxes_median_home_value": 2124
+  },
+  "Oklahoma": {
+     "effective_real_estate_tax_rate": 0.009,
+     "annual_taxes_on_194k_home": 1737,
+     "median_home_value": 125800,
+     "annual_taxes_median_home_value": 1129
+  },
+  "Oregon": {
+     "effective_real_estate_tax_rate": 0.0104,
+     "annual_taxes_on_194k_home": 2006,
+     "median_home_value": 265700,
+     "annual_taxes_median_home_value": 2755
+  },
+  "Pennsylvania": {
+     "effective_real_estate_tax_rate": 0.0158,
+     "annual_taxes_on_194k_home": 3054,
+     "median_home_value": 170500,
+     "annual_taxes_median_home_value": 2691
+  },
+  "Rhode Island": {
+     "effective_real_estate_tax_rate": 0.0166,
+     "annual_taxes_on_194k_home": 3206,
+     "median_home_value": 242200,
+     "annual_taxes_median_home_value": 4013
+  },
+  "South Carolina": {
+     "effective_real_estate_tax_rate": 0.0057,
+     "annual_taxes_on_194k_home": 1108,
+     "median_home_value": 148600,
+     "annual_taxes_median_home_value": 851
+  },
+  "South Dakota": {
+     "effective_real_estate_tax_rate": 0.0132,
+     "annual_taxes_on_194k_home": 2550,
+     "median_home_value": 152700,
+     "annual_taxes_median_home_value": 2012
+  },
+  "Tennessee": {
+     "effective_real_estate_tax_rate": 0.0074,
+     "annual_taxes_on_194k_home": 1429,
+     "median_home_value": 151700,
+     "annual_taxes_median_home_value": 1120
+  },
+  "Texas": {
+     "effective_real_estate_tax_rate": 0.0183,
+     "annual_taxes_on_194k_home": 3544,
+     "median_home_value": 151500,
+     "annual_taxes_median_home_value": 2775
+  },
+  "Utah": {
+     "effective_real_estate_tax_rate": 0.0066,
+     "annual_taxes_on_194k_home": 1280,
+     "median_home_value": 238300,
+     "annual_taxes_median_home_value": 1576
+  },
+  "Vermont": {
+     "effective_real_estate_tax_rate": 0.0183,
+     "annual_taxes_on_194k_home": 3544,
+     "median_home_value": 220600,
+     "annual_taxes_median_home_value": 4040
+  },
+  "Virginia": {
+     "effective_real_estate_tax_rate": 0.008,
+     "annual_taxes_on_194k_home": 1550,
+     "median_home_value": 255800,
+     "annual_taxes_median_home_value": 2049
+  },
+  "Washington": {
+     "effective_real_estate_tax_rate": 0.0103,
+     "annual_taxes_on_194k_home": 1996,
+     "median_home_value": 286800,
+     "annual_taxes_median_home_value": 2958
+  },
+  "West Virginia": {
+     "effective_real_estate_tax_rate": 0.0059,
+     "annual_taxes_on_194k_home": 1132,
+     "median_home_value": 111600,
+     "annual_taxes_median_home_value": 653
+  },
+  "Wisconsin": {
+     "effective_real_estate_tax_rate": 0.0194,
+     "annual_taxes_on_194k_home": 3756,
+     "median_home_value": 169300,
+     "annual_taxes_median_home_value": 3286
+  },
+  "Wyoming": {
+     "effective_real_estate_tax_rate": 0.0061,
+     "annual_taxes_on_194k_home": 1186,
+     "median_home_value": 204900,
+     "annual_taxes_median_home_value": 1256
+  }
+}
